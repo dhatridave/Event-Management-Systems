f07d439a9f6fdf5d26cedb3d0a4e9d3046aba6e3
dhatri <jachu2101@gmail.com>
2022-04-09 18:36:38 +0530

    admin teampleats

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,16 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from atexit import register
-from unicodedata import name
+
 from django.contrib import admin
 from django.urls import path, include
 
-from myapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
-    
-  
+    path('org/', include('myweb.urls')),
+
 ]
--- myweb/models.py
+++ myweb/models.py
@@ -6,7 +6,7 @@ from pyexpat import model
 
 class Event(models.Model):
 
-
         ename = models.CharField(max_length=50)
         edescription = models.TextField(max_length=500)
         famount = models.IntegerField()
+
